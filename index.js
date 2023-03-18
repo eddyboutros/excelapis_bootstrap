c311a82d49a6de48476da5d3e1430d7e75468557
eddyboutros <eddy.boutros.36@gmail.com>
2023-03-18 15:07:33 +0200

    initial excel apis

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+const excel = require('read-excel-file/node');
+app.use(cors());
+
+app.get('/getWelcomePageIconsInfo', function (req, res) {
+  const directoryPath = process.cwd();
+
+  excel(directoryPath + "/welcome-page-icons.xlsx").then((rows) => {
+    res.send(rows);
+  }).catch((error) => {
+    res.send("File does not exist!");
+  });
+});
+
+app.get('/getWelcomeParagraph', function (req, res) {
+  const directoryPath = process.cwd();
+
+  excel(directoryPath + "/welcome-paragraph.xlsx").then((rows) => {
+    res.send(rows);
+  }).catch((error) => {
+    res.send("File does not exist!");
+  });
+});
+
+app.get('/getInformationAfterWelcome', function (req, res) {
+  const directoryPath = process.cwd();
+
+  excel(directoryPath + "/information-after-welcome.xlsx").then((rows) => {
+    res.send(rows);
+  }).catch((error) => {
+    res.send("File does not exist!");
+  });
+});
+
+app.get('/getMembers', function (req, res) {
+  const directoryPath = process.cwd();
+
+  excel(directoryPath + "/members-information.xlsx").then((rows) => {
+    res.send(rows);
+  }).catch((error) => {
+    res.send("File does not exist!");
+  });
+});
+
+app.get('/getServices', function (req, res) {
+  const directoryPath = process.cwd();
+
+  excel(directoryPath + "/services.xlsx").then((rows) => {
+    res.send(rows);
+  }).catch((error) => {
+    res.send("File does not exist!");
+  });
+});
+
+/*process.env.PORT is for render.com. Environment Variable PORT is created*/
+const port = /*process.env.PORT*/3000;
+
+app.listen(port, function (){
+  console.log('Server is running on ' + port);
+});
